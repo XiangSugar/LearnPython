f61c48b89bb84680b4ffa819ca3742b9831db6fd
XiangSu <suxiang@hust.edu.cn>
2018-03-19 20:12:09 +0800

    the use of map() and reduce()

--- map_reduce.py
+++ map_reduce.py
@@ -35,6 +35,30 @@ def fn(x,y):
 reduce(fn,[1,3,5,7,9])
 
 #---------------------------------------
+from functools import reduce
+def fn(x,y):
+    return 10 * x + y
+
+def char2num(s):
+    digits = {'0'=0, '1'=1, '3'=3, '4'=4, '5'=5, '6'=6, '7'=7, '8'=8, '9'=9}
+    return digits[s]
+
+numb = reduce(fn, map(char2num, '13579')
+print(numb)
+
+#-------------------------------------------
+from functools import reduce
+
+DIGITS = {'0'=0, '1'=1, '3'=3, '4'=4, '5'=5, '6'=6, '7'=7, '8'=8, '9'=9}
+
+def str2int(s):
+    def fn(x,y):
+        return 10 * x + y
+    def char2num(ss):
+        return DIGITS[ss]
+    return reduce(fn, map(char2num, s))
+
+#-------------------------------------------
 
 
 
