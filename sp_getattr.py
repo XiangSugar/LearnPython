572407c5f8d21ec0b755769abdc90c8d474fd671
XiangSu <suxiang@hust.edu.cn>
2018-03-27 20:02:36 +0800

    about special __getattr__

---
+++ sp_getattr.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+class Student(object):
+    def __init__(self):
+        self.name = 'Xiang Su'
+
+    def __getattr__(self, attr):
+        if attr == 'score':
+            return 99
+        raise AttributeError('\'Student\' object has no attribute \'%s\'' % attr)
+
+s = Student()
+print(s.name)
+print(s.score)
+print(s.age)
