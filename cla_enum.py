383baaf79f09959c53838ba8f247a08fb0bc7c41
XiangSu <suxiang@hust.edu.cn>
2018-03-28 08:53:56 +0800

    add some codes

--- cla_enum.py
+++ cla_enum.py
@@ -8,3 +8,9 @@ for name, member in Month.__members__.items():
     print(name, '=>', member, ',', member.value)
 
 print(Month.Jan)
+
+Month1 = Month.Jan
+print(Month1)
+print(Month['May'])
+print(Month.Jun.value)
+print(Month(9))
