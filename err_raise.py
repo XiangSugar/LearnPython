14828a321803b4f64300df9e30dbadcdfcf0c4b6
XiangSu <suxiang@hust.edu.cn>
2018-03-29 15:46:07 +0800

    about error raise

---
+++ err_raise.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# error_raise.py
+
+#-------------------------------------------
+class FooError(ValueError):
+    pass
+
+def foo(s):
+    n = int(s)
+    if n == 0:
+        raise FooError('invalid value: %s' % s)
+    return 10 / n
+
+foo('0')
+
+
+#---------------------------------------------------
+def foo(s):
+    n = int(s)
+    if n == 0:
+        raise ValueError('invalid value: %s' % s)
+    return 10 / n
+
+def bar():
+    try:
+        foo('0')
+    except ValueError as e:
+        print('ValueError!')
+        raise  #raise 后面不带参数表示把错误原样抛出
+    
+bar()
