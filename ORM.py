523e39dda5cad53e3f89ea5937613de4860d5f34
XiangSu <suxiang@hust.edu.cn>
2018-03-29 15:06:06 +0800

    modified the format

--- ORM.py
+++ ORM.py
@@ -5,40 +5,32 @@ class Field(object):
     def __init__(self, name, column_type):
         self.name = name
         self.column_type = column_type
-
     def __str__(self):
         return '<%s:%s>' % (self.__class__.__name__, self.name)
-
 
 class StringField(Field):
     def __init__(self, name):
         super(StringField, self).__init__(name, 'varchar(100)')
 
-
 class IntegerField(Field):
     def __init__(self, name):
         super(IntegerField, self).__init__(name, 'bigint')
-
 
 class ModelMetaclass(type):
     def __new__(cls, name, bases, attrs):
         # 排除对Model类的修改
         if name == 'Model':
             return type.__new__(cls, name, bases, attrs)
-
         print('Found model: %s' % name)
-
         mappings = dict()
         for k, v in attrs.items():
             if isinstance(v, Field):
                 print('Found mapping: %s ==> %s' % (k, v))
                 mappings[k] = v
-        
         for k in mappings.keys():
             attrs.pop(k)
         attrs['__mappings__'] = mappings # 保存属性和列的映射关系
         attrs['__table__'] = name # 假设表名和类名一致
-
         return type.__new__(cls, name, bases, attrs)
 
 class Model(dict, metaclass = ModelMetaclass):
@@ -51,7 +43,7 @@ class Model(dict, metaclass = ModelMetaclass):
             raise AttributeError(r"'Model' object has no arrtibute '%s'" % key)
     def __setattr__(self, key, value):
         self[key] = value
-        
+
     def save(self):
         fields = []
         params = []
