a68a6cfd34aee45edfeec89293fbd3e594bf8274
XiangSu <suxiang@hust.edu.cn>
2018-04-02 21:48:09 +0800

    This is about StringIO

---
+++ StringIO.py
@@ -0,0 +1,17 @@
+# -*- coding :utf-8 -*-
+# This is about StringIO
+
+from io import StringIO
+
+f = StringIO()
+f.write('Hello')
+f.write(' ')
+f.write('world!')
+print(f.getvalue())  # getvalue() 用于获得写入后的 str
+
+f1 = StringIO('Hello!\nHi!\nGoodBye!')
+while True:
+    s = f1.readline()
+    if s == '':
+        break
+    print(s.strip())
