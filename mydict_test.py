44b8210ad2a1301980b0e89caefe26c9814c51bf
XiangSu <suxiang@hust.edu.cn>
2018-04-02 11:26:40 +0800

    add some discriptions

--- mydict_test.py
+++ mydict_test.py
@@ -38,6 +38,13 @@ class TestDict(unittest.TestCase):
     def tearDown(self):
         print('tearDown...')
 
+#可以在单元测试中编写两个特殊的setUp()和tearDown()方法。
+# 这两个方法会分别在每调用一个测试方法的前后分别被执行。
+#setUp()和tearDown()方法有什么用呢？设想你的测试需要启动一个数据库，
+# 这时，就可以在setUp()方法中连接数据库，在tearDown()方法中关闭数据库，
+# 这样，不必在每个测试方法中重复相同的代码
+
+
 # 这两行代码可以把该 .py 文件当作正常的脚本运行
 if __name__ == '__main__':
     unittest.main()
