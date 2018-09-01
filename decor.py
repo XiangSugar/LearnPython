48552c3b89fdd029321939456934032a7e1b2810
Su,xiang <33318078+XiangSugar@users.noreply.github.com>
2018-09-01 16:31:29 +0800

    Update decor.py
    add one more case  and some detailed comments about decorator

--- decor.py
+++ decor.py
@@ -19,7 +19,25 @@ def Now():
 
 print(Now())
 
-#----------------------------------------
+# ---------------------------------------
+def log(text):
+    def decorator(func):
+        def wrapper(*args, **kw):
+            printf('%s %s()' % (text, func.__name__))
+            return func(*args, **kw)
+        return wrapper
+    return decorator
+
+@log('execute')
+def now():
+    print('2018-9-1')
+
+# -------------------------------------------------------------------------------------------
+# 以上经过 decorator 装饰之后的函数，它们的__name__已经从原来的'now'变成了'wrapper'：
+# 因为返回的那个wrapper()函数名字就是'wrapper'，所以，需要把原始函数的__name__等属性复制到wrapper()函数中，
+# 否则，有些依赖函数签名的代码执行就会出错
+# Python 内置的functools.wraps就是干这个事的，所以，一个完整的 decorator 的写法如下：
+# -------------------------------------------------------------------------------------------
 import functools
 
 def log_1(text):
@@ -36,3 +54,5 @@ def now_1():
     print('2018-3-20 15:17')
 
 print(now_1())
+
+# 在装饰器的返回函数的定义之前加上  @functools.wraps(func)  即可
